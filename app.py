c17b1576759b1f9b5bd62440000bfe781ab315db
Jeff <jlin02@stuy.edu>
2019-10-22 18:44:25 -0400

    Progress on home discovery page in HTML.

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import cgi
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
+from flask import url_for
 
 app = Flask(__name__)
 @app.route("/")
