309acca7cd6de9745476cc37ef965fc08e271326
Jeff <jlin02@stuy.edu>
2019-10-22 14:42:16 -0400

    Added template for flask and empty HTML page.

---
+++ app.py
@@ -0,0 +1,27 @@
+# Ahmed Sultan, Jeff Lin, Leia Park [BunnyTruffles]
+# SoftDev1 PD 9
+# P00
+# 2019-10-28
+
+import cgi
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+@app.route("/")
+def occupyflaskst():
+    return render_template('discover.html')
+
+# @app.route("/auth")
+# def authenticate():
+#     print(app)                      # Prints out Flask app name
+#     print(request)                  # Prints out Flask app web address
+#     print(request.args)             # Prints out dictionary with form information
+#     return render_template(         # Renders new page templates
+#
+#     )
+
+if __name__ == "__main__":
+  app.debug = True
+  app.run()
